719d18e13482905b8a2b8ecc2fd797993bf6e86b
Batchuluun0605 <23LP2014@nest.edu.mn>
2023-10-12 23:00:13 +0800

    add readme

---
+++ index.js
@@ -0,0 +1,59 @@
+/* 
+1
+12
+123
+1234
+12345
+*/
+// let too = [ 1, 2, 3, 4, 5];
+// let d = "";
+// for (i = 0; i < too.length; i++){
+//     for (j = 0; j <= i; j++){
+//         d = d + too[i]
+//     }
+//     console.log(d);
+//     d = ""
+// }
+
+
+/*
+1
+12
+123
+1234
+12345
+*/
+// let too = [ 1, 2, 3, 4, 5];
+// let d = ""
+// for ( i = 0; i < too.length; i++){
+//     for (j = 0; j <= i; j++){
+//       d = d + "" + too[j]
+//     }
+//     console.log(d);
+//     d = ""
+// }
+
+let too = [1, 2, 3, 4, 5];
+d = "";
+for (i = 0; i < too.length; i++){
+    for ( j = 0; j <= i; j++ ){
+        d= d + too[j]
+       }
+       console.log(d);
+       d = ""
+    }
+    
+    
+// }
+
+// let too = [1, 2, 3, 4, 5];
+// d = "";
+// for (i = 0; i < too.length; i++){
+//     for ( j = 0; j <= i; j++ ){
+//         d= d + too[j]
+//        }
+//        console.log(d);
+//        d = ""
+// }
+
+
